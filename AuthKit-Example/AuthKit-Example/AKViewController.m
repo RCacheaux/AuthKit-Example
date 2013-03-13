6a816d7d06b7ad2d91d67bf4a0b9b5821424bd8f
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2013-03-12 21:46:16 -0500

    Removed VC's nib and updated bundle display name

--- AuthKit-Example/AuthKit-Example/AKViewController.m
+++ AuthKit-Example/AuthKit-Example/AKViewController.m
@@ -1,11 +1,3 @@
-//
-//  AKViewController.m
-//  AuthKit-Example
-//
-//  Created by Rene Cacheaux on 3/12/13.
-//  Copyright (c) 2013 RCach Apps. All rights reserved.
-//
-
 #import "AKViewController.h"
 
 @interface AKViewController ()
@@ -14,16 +6,23 @@
 
 @implementation AKViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)loadView {
+  self.view = [[UIView alloc] init];
+  self.view.backgroundColor = [UIColor redColor];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  UILabel *screenIdentifier =
+      [[UILabel alloc] initWithFrame:CGRectMake(100.0f, 100.0f, 200.0f, 30.0f)];
+  screenIdentifier.text = @"LOGIN";
+  screenIdentifier.backgroundColor = [UIColor clearColor];
+  [self.view addSubview:screenIdentifier];
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
